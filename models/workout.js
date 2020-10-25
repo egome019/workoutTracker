8e8202ec5e11e7b6466d4d0b9639cca80802d6ee
erik G <egomezfax19@gmail.com>
2020-10-24 22:56:57 -0700

    created initial schema

--- models/workout.js
+++ models/workout.js
@@ -2,7 +2,38 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const workoutSchema = new Schema({
-    // code goes here...
+    exercise:[{
+        name: {
+            type: String,
+            required: "Enter the name of the exercise.",
+            trim: true
+        },
+        type: {
+            type: String,
+            required: "Enter an the type of exercise performed.",
+            trim: true
+        },
+        weight: {
+            type: Number
+        },
+        sets: {
+            type: Number
+        },
+        reps: {
+            type: Number
+        },
+        duration: {
+            type: Number,
+            required: "In minutes, enter the duration of the exercise."
+        },
+        distance: {
+            type: Number
+        }
+    }],
+    day: {
+        type: Date,
+        default: new Date()
+    }
 });
 
 const workout = mongoose.model("workout", workoutSchema);
