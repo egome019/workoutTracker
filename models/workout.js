d1cca59f58db8e3beda76a30eb7b4387a4817de8
erik G <egomezfax19@gmail.com>
2020-10-31 00:35:10 -0700

    trying to deploy

--- models/workout.js
+++ models/workout.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const workoutSchema = new Schema({
-    exercise:[{
+    exercises:[{
         name: {
             type: String,
             required: "Enter the name of the exercise.",
